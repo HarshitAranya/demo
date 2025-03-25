322f243a343169b7be8fb8a98e9f267d8230d17a
Harshit Aranya <81676573+HarshitAranya@users.noreply.github.com>
2025-03-25 14:01:11 +0530

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Deploying HTML files..."
+chown -R nginx:nginx /usr/share/nginx/html
+chmod -R 755 /usr/share/nginx/html
+
+echo "Restarting Nginx..."
+systemctl restart nginx
+
+echo "Deployment completed!"
